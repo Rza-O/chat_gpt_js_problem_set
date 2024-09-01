fa66a05ed0a8a6df953d1cb742811ca10634c5c0
appoclayptor <shahreza1414@gmail.com>
2024-09-01 06:05:04 +0600

    Solved third problem

---
+++ beginner-level_conditional_problems/grades.js
@@ -0,0 +1,21 @@
+//Grade Classification: Write a program that takes a numeric grade (0-100) and prints out the corresponding letter grade (A, B, C, D, F).
+
+let marks = 38;
+
+if(marks >= 60){
+    if( marks >= 90){
+        console.log('You have gotten A')
+    }
+    else if(marks >= 80){
+        console.log('You have gotten B')
+    }
+    else if(marks >= 70){
+        console.log('You have gotten C')
+    }
+    else{
+        console.log('You have gotten D')
+    }
+}
+else{
+    console.log('You have failed. Better Luck next time')
+}
